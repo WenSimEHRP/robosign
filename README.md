4fcf2b6d79bb132d081fce7425f256785c497a72
Jeremy Gao <wensimehrp@gmail.com>
2026-04-24 23:09:54 -0700

    feat: init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Robosign
+
+A.K.A. Radish sign.
+
+Tool for making station signs.
+
+## Usage
+
+Open the site then make it.
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Web development environment with Bun";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+          ];
+        };
+
+        packages.default = pkgs.bun;
+      }
+    );
+}
